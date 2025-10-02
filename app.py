9ccec54884293d2c4ab801a834487acb9b874911
Biprayan Choudhuri <biprayanchoudhuri58@gmail.com>
2025-10-02 09:42:53 +0530

    Initial broken version

---
+++ app.py
@@ -0,0 +1,4 @@
+def add(a,b):   return a + b + 1  # bad spacing, and wrong logic
+
+def subtract(a, b):
+ return a - b  # bad indentation
---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5  # This will FAIL because add returns 6
+
+def test_subtract():
+    assert subtract(5, 2) == 3  # This will PASS
